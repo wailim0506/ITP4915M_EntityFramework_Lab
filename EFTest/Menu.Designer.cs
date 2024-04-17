cbd03605a7341da38f6e82f374d2ecbceab27054
wailim0506 <wailim0506@icloud.com>
2024-04-17 11:12:37 +0800

    s

--- EFTest/Menu.Designer.cs
+++ EFTest/Menu.Designer.cs
@@ -125,7 +125,7 @@ namespace EFTest
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4; 
+        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label1;
     }
